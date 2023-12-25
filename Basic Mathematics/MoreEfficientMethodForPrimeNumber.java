0127fbebb2e4d41aa4b0ad98907a99c567f75526
RohuTheCoder <rohugb777999@gmai.com>
2023-12-25 19:23:30 +0530

    More Efficient Method for Prime Number

---
+++ Basic Mathematics/MoreEfficientMethodForPrimeNumber.java
@@ -0,0 +1,21 @@
+class MoreEfficientMethodForPrimeNumber
+{
+    public static void main(String[] args)
+    {
+        boolean result = isPrime(1031);
+        System.out.println("The given number is Prime?(true/false): "+result);
+    }
+    static boolean isPrime(int n)
+    {
+        if(n==1)
+            return false;
+        if(n==2||n==3)
+            return true;
+        if(n%2==0||n%3==0)
+            return false;
+        for(int i=5;i*i<=n;i=i+6)
+            if(n%i==0||n%(i+2)==0)
+                return false;
+        return true;
+    }
+}
