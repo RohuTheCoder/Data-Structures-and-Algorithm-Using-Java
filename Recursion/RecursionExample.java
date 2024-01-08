5356e544e871a07d209741ca8933786aa90c12a1
RohuTheCoder <rohugb777999@gmai.com>
2024-01-08 20:06:36 +0530

    Simple example for recursion

---
+++ Recursion/RecursionExample.java
@@ -0,0 +1,13 @@
+public class RecursionExample
+{
+    static void fun1(int n)
+    {
+        if(n==0)
+        return;
+        System.out.println("hello");
+        fun1(n-1);
+    }
+    public static void main(String[] args) {
+        fun1(5);
+    }
+}
