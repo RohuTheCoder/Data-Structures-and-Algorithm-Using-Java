58522c7e877fdccce10dad224e64392ee8a2711e
RohuTheCoder <rohugb777999@gmai.com>
2024-01-14 07:37:19 +0530

    Generate Subsets of a given String

---
+++ Recursion/GenerateSubsets.java
@@ -0,0 +1,18 @@
+public class GenerateSubsets {
+    public static void main(String[] args) {
+        String s = "ABC";
+        String curr = " ";
+        int i = 0;
+        subsets(s,curr,i);
+    } 
+    static void subsets(String s,String curr,int i)
+    {
+        if(i==s.length())
+        {
+            System.out.println(curr);
+            return;
+        }
+        subsets(s,curr,i+1);
+        subsets(s,curr+s.charAt(i),i+1);
+    }   
+}
