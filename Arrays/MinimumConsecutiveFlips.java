3c443f1992214489d1f67f9735491e8bfdfefa56
rohini.gb <rohugb777999@gmail.com>
2024-02-06 20:09:07 +0530

    Minimum Consecutive Flips

---
+++ Arrays/MinimumConsecutiveFlips.java
@@ -0,0 +1,29 @@
+public class MinimumConsecutiveFlips {
+    public static void main(String[] args) {
+     int arr[] = {0,0,1,1,0,0,1,1,0,1};
+     printGroups(arr);   
+    }
+    static void printGroups(int arr[])
+    {
+        int n = arr.length;
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]!=arr[i-1])
+            {
+                if(arr[i]!=arr[0])
+                    System.out.print("From "+i+" to ");
+                else
+                {
+                    System.out.print((i-1));
+                    System.out.println();
+                }
+            }
+        }
+        if(arr[n-1]!=arr[0])
+        {
+            System.out.print((n-1));
+            System.out.println();
+        }
+    }
+    
+}
