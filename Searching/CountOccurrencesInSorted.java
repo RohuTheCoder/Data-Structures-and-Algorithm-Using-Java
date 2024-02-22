859cd53d1e7f7b90aad94bb0a28f2cb402a00f8c
rohini.gb <rohugb777999@gmail.com>
2024-02-22 21:15:00 +0530

    Count Occurrences in Sorted Array

---
+++ Searching/CountOccurrencesInSorted.java
@@ -0,0 +1,61 @@
+public class CountOccurrencesInSorted {
+
+    public static void main(String[] args) {
+      int arr[] = {10,20,20,20,40,40};
+      int n = arr.length;
+      int x = 20;
+      int count = countOcc(arr, n, x);
+      System.out.println(count);  
+    }
+    static int countOcc(int arr[],int n,int x)
+    {
+        int first = firstOcc(arr, n, x);
+        if(first==-1)
+            return 0;
+        else
+            return (lastOcc(arr, n, x)-first+1);
+    }
+    static int firstOcc(int arr[],int n,int x)
+    {
+        int low = 0;
+        int high = n-1;
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            if(arr[mid]>x)
+                high = mid-1;
+            else if(arr[mid]<x)
+                low = mid+1;
+            else
+            {
+                if(mid==0||arr[mid]!=arr[mid-1])
+                    return mid;
+                else
+                    high = mid+1;
+            }
+        }
+        return -1;
+    }
+    static int lastOcc(int arr[],int n,int x)
+    {
+        int low = 0;
+        int high = n-1;
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            if(arr[mid]<x)
+                low = mid+1;
+            else if(arr[mid]>x)
+                high = mid-1;
+            else
+            {
+                if(mid==n-1||arr[mid]!=arr[mid+1])
+                    return mid;
+                else
+                    low = mid+1;
+            }
+        }
+        return -1;
+    }
+}
+
