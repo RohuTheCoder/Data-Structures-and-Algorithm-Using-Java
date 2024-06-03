0f290405eb55d8d9f21827ec879fde5cf5dc8972
rohini.gb <rohugb777999@gmail.com>
2024-06-03 21:45:06 +0530

    Sorting a List in Increasing and decreasing Order

---
+++ Sorting/SortingList.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class SortingList
+{
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(5);
+        list.add(20);
+        Collections.sort(list);
+        System.out.println(list);
+        Collections.sort(list,Collections.reverseOrder());
+        System.out.println(list);
+
+    }
+}
